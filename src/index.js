9ded49e7e90630d2a49dc822110c33ac2a8ef5e8
Hanwhy <dngur515@naver.com>
2023-02-10 14:48:00 +0000

    Update 4 files

--- src/index.js
+++ src/index.js
@@ -1,43 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-//import App from "./App";
-
+import Main from "./Main";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
-function Main(){
-  function click()
-  {
-    window.location.replace('/Subway');
-  }
-  return(
-    <div>
-      <h1>Hello customer</h1>
-      <h2>Subway station App!</h2>
-      
-      <button style={{
-        width:"200px",
-        height:"100px",
-        borderRadius: 10,
-        backgroundColor:"#1E90FF",
-        color:"white"
-      }}
-      onClick={click}>
-        시작
-      </button>
-    </div>
-  );
-}
-
-function Subway(){
-  var api = "	http://data.seoul.go.kr/dataList/datasetView.do?infId=OA-12764&srvType=A&serviceKind=1&currentPageNo=1";
-  return(
-    <div>
-      <h1>호선</h1>
-    </div>
-  );
-}
 
 root.render(
   <StrictMode>
