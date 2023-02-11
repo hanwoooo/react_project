585ea536b92cf03bb9d1c13b77196bc5d493005f
Hanwhy <dngur515@naver.com>
2023-02-11 14:08:26 +0000

    css & Subway Page update!!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles.css";
 import Main from "./Main";
 import Subway from "./Subway";
 
+// 전체 App을 다루는 js
+// router를 사용해서 주소 값을 보고 페이지를 바꾼다.
 const App = () => {
   return (
     <div className="App">
--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,23 @@
 import React from "react";
+import "./styles.css";
+// 메인 화면
 function Main() {
-  //
+  // 버튼 클릭 이벤트
   function click() {
-    window.location.replace("/Subway");
+    //href는 뒤로가기가 가능, replace는 뒤로가기 불가능
+    window.location.href="/Subway";
   }
   return (
     <div>
       <h1>Hello customer</h1>
+      <br />
       <h2>Subway station App!</h2>
+      <br />
+      <br />
+      <br />
+      <br />
 
-      <button
-        style={{
-          width: "200px",
-          height: "100px",
-          borderRadius: 10,
-          backgroundColor: "white",
-          color: "#1E90FF",
-        }}
-        onClick={click}
-      >
+      <button id="btn" onClick={click}>
         시작
       </button>
     </div>
