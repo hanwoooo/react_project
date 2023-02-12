f9a010a641b8c53ea8c5bf8458e541377430baf7
Hanwhy <dngur515@naver.com>
2023-02-12 05:54:20 +0000

    Update Subway.js and styles.css

--- src/Subway.js
+++ src/Subway.js
@@ -2,16 +2,24 @@ import React from "react";
 import image from "./subway_line.PNG";
 import "./styles.css";
 function Subway() {
-  var api =
-    "http://data.seoul.go.kr/dataList/datasetView.do?infId=OA-12764&srvType=A&serviceKind=1&currentPageNo=1";
   return (
     <div>
-      <img src={image} />
+      <img id="img" src={image} />
       <div className="Subway">
-        <h1>출발지</h1>
-        <h1>목적지</h1>
+        <h2>
+          <br />
+          출발지
+        </h2>
+        <input id="start" placeholder="left click" disabled></input>
+        <h2>목적지</h2>
+        <br />
+        <input id="end" placeholder="right click" disabled></input>
+        <h3>걸리는 시간</h3>
+        <input id="time" disabled></input>
         <h2>최소요금</h2>
+        <input id="money" disabled></input>
         <h2>최소환승</h2>
+        <input id="change" disabled></input>
       </div>
     </div>
   );
